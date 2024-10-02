ec4e915da4fe8d5a5eae3cf67d4281c1facfe305
David MARTINS <david.joaquim.martins@gmail.com>
2024-10-02 03:44:50 +0200

    La bonne paye 2

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,47 @@
 window.onload = function() {
 
-var payementDu=[0,0]
-fetch('/total')
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(item => {
-        // Mise à jour de l'affichage pour chaque nom
-        if (item._id === "Lola") {
-            payementDu[0]=item.totalSomme
-            console.log(item.totalSomme)
-        }
-        if (item._id === "David") {
-            payementDu[1]=item.totalSomme
-            console.log(item.totalSomme)
-        }
-        if(payementDu[0] > payementDu[1]) {
-            var doitPayer = payementDu[0] - payementDu[1];
-            document.getElementById('david').innerText = `David doit : ${doitPayer} €`;
-            document.getElementById('lola').innerText = `Lola est zen !! `;
-        } else if(payementDu[0] < payementDu[1]) {
-            var doitPayer = payementDu[1] - payementDu[0];
-            document.getElementById('lola').innerText = `Lola doit : ${doitPayer} €`;
-            document.getElementById('david').innerText = `David est zen !!`;
-        } else if(payementDu[0] === payementDu[1]) { 
-            // document.getElementById('david').innerText = ` `;
-            document.getElementById('lola').innerText = `On est pas mal !!! `;
-        }
-      });
-    })
-    .catch(error => console.error('Erreur:', error));
+    var payementDu = [0, 0];
 
-    
+    // Récupérer les données depuis le serveur
+    fetch('/total')
+        .then(response => response.json())
+        .then(data => {
+            // Réinitialiser les montants avant de mettre à jour
+            let totalLola = 0;
+            let totalDavid = 0;
 
-}
+            // Parcourir les données récupérées
+            data.forEach(item => {
+                if (item._id === "Lola") {
+                    totalLola = item.totalSomme;
+                }
+                if (item._id === "David") {
+                    totalDavid = item.totalSomme;
+                }
+            });
+
+            // Mettre à jour payementDu
+            payementDu[0] = totalLola;
+            payementDu[1] = totalDavid;
+
+            // Log des résultats pour débogage
+            console.log("Lola: ", payementDu[0], "David: ", payementDu[1]);
+
+            // Mettre à jour l'affichage
+            if (payementDu[0] > payementDu[1]) {
+                var doitPayer = payementDu[0] - payementDu[1];
+                document.getElementById('david').style.display = "block";
+                document.getElementById('david').innerText = `David doit : ${doitPayer} €`;
+                document.getElementById('lola').innerText = `Lola est zen !!`;
+            } else if (payementDu[0] < payementDu[1]) {
+                var doitPayer = payementDu[1] - payementDu[0];
+                document.getElementById('david').style.display = "block";
+                document.getElementById('lola').innerText = `Lola doit : ${doitPayer} €`;
+                document.getElementById('david').innerText = `David est zen !!`;
+            } else if (payementDu[0] === payementDu[1]) {
+                document.getElementById('lola').innerText = `On est pas mal !!!`;
+                document.getElementById('david').style.display = "none";
+            }
+        })
+        .catch(error => console.error('Erreur:', error));
+};
