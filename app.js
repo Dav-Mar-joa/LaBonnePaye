2387a79a544731ee10f363624ece2a02f1878306
David MARTINS <david.joaquim.martins@gmail.com>
2024-10-02 02:55:53 +0200

    La bonne paye 2

--- app.js
+++ app.js
@@ -3,24 +3,20 @@ const app = express();
 const port = 3000;
 const http = require('http');
 const path = require('path');
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 const server = http.createServer(app);
 require('dotenv').config();
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Définir Pug comme moteur de vue
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.json());
+
+// Middleware pour le traitement des données de formulaire
 app.use(express.urlencoded({ extended: true }));
 
 const connectionString = process.env.MONGODB_URI;
-
-
 const client = new MongoClient(connectionString);
-const dbName = process.env.mongodb_dbname;
+const dbName = process.env.MONGODB_DBNAME;
 
 let db;
 async function connectDB() {
@@ -35,9 +31,55 @@ async function connectDB() {
 connectDB();
 
 app.get('/', (req, res) => {
+
     res.render('index');
 });
 
+app.post('/payement', async (req, res) => {
+    console.log("Données reçues :", req.body); // Log des données reçues
+
+    const payement = {
+        name: req.body.name ,
+        date: new Date(req.body.date), 
+        description: req.body.description || "Aucune description",
+        somme: parseFloat(req.body.somme) 
+    };
+
+    console.log("Données avant insertion :", payement); // Log avant l'insertion
+
+    try {
+        const collection = db.collection(process.env.MONGODB_COLLECTION);
+        await collection.insertOne(payement);
+        console.log("Document inséré avec succès !");
+        res.redirect('/?success=true'); // Redirection avec un paramètre de succès
+    } catch (err) {
+        console.error('Erreur lors de l\'ajout dU PAYEMENT :', err);
+        res.status(500).send('Erreur lors de l\'ajout de la tâche');
+    }
+});
+
+app.get('/total', async (req, res) => {
+    try {
+        const collection = db.collection(process.env.MONGODB_COLLECTION);
+
+        // Agrégation pour grouper par 'name' et calculer la somme des 'somme'
+        const result = await collection.aggregate([
+            {
+                $group: {
+                    _id: "$name", // Grouper par le champ 'name'
+                    totalSomme: { $sum: "$somme" } // Calculer la somme des champs 'somme'
+                }
+            }
+        ]).toArray();
+
+        console.log("Résultat de l'agrégation :", result);
+        res.json(result); // Envoyer le résultat sous forme JSON
+    } catch (err) {
+        console.error('Erreur lors de l\'agrégation :', err);
+        res.status(500).send('Erreur lors de la récupération des données');
+    }
+});
+
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+    console.log(`Serveur lancé sur le port : ${port}`);
 });
---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+
+var payementDu=[0,0]
+fetch('/total')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(item => {
+        // Mise à jour de l'affichage pour chaque nom
+        if (item._id === "Lola") {
+            payementDu[0]=item.totalSomme
+            console.log(item.totalSomme)
+        }
+        if (item._id === "David") {
+            payementDu[1]=item.totalSomme
+            console.log(item.totalSomme)
+        }
+        if(payementDu[0] > payementDu[1]) {
+            var doitPayer = payementDu[0] - payementDu[1];
+            document.getElementById('lola').innerText = `Lola doit : ${doitPayer} €`;
+            document.getElementById('david').innerText = `0 €`;
+        } else if(payementDu[0] < payementDu[1]) {
+            var doitPayer = payementDu[1] - payementDu[0];
+            document.getElementById('david').innerText = `David doit : ${doitPayer} €`;
+            document.getElementById('lola').innerText = `0 €`;
+        } else if(payementDu[0] === payementDu[1]) { 
+            document.getElementById('david').innerText = `On est pas mal !! `;
+            document.getElementById('lola').innerText = `Les comptes sont OK !!! `;
+        }
+      });
+    })
+    .catch(error => console.error('Erreur:', error));
+
+    
+
+}
